accebf7b84cf49c2eae3ac99155c8270175e1fa7
unknown <jeff.gbeho@gmail.com>
2024-10-29 19:19:27 +0100

    configuration new dependencies

--- api/src/main/java/com/marketplace/test/TestController.java
+++ api/src/main/java/com/marketplace/test/TestController.java
@@ -1,11 +1,15 @@
 package com.marketplace.test;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Test", description = "API de test")
 public class TestController {
     @GetMapping("/test")
+    @Operation(summary = "Test endpoint")
     public String test() {
         return "Application is working !";
     }
