7ee89e8099e21bc52b7a15019ed7d5e4d26f9d6f
lisadev <isabelle.moriamez@orange.fr>
2024-11-05 14:15:43 +0100

    feature:crud product

---
+++ api/src/main/java/com/marketplace/service/impl/ProductServiceImpl.java
@@ -0,0 +1,55 @@
+package com.marketplace.service.impl;
+
+import com.marketplace.entity.Product;
+import com.marketplace.repository.ProductRepository;
+import com.marketplace.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public Product updateProduct(Long id, Product productDetails) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    existingProduct.setNom(productDetails.getNom());
+                    existingProduct.setDescription(productDetails.getDescription());
+                    existingProduct.setPhoto(productDetails.getPhoto());
+                    existingProduct.setPrixUnitaire(productDetails.getPrixUnitaire());
+                    existingProduct.setValeurNutri(productDetails.getValeurNutri());
+                    existingProduct.setListeIngrédients(productDetails.getListeIngrédients());
+                    existingProduct.setQteStock(productDetails.getQteStock());
+                    existingProduct.setSeuilCritique(productDetails.getSeuilCritique());
+                    return productRepository.save(existingProduct);
+                })
+                .orElseThrow(() -> new RuntimeException("Produit non trouvé avec l'ID : " + id));
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
+
+
