5a05692f90318a18a43286794f363c6c436c7bc0
Pauline SOUBRIE <pauline_soubrie@hotmail.fr>
2024-11-05 11:47:30 +0100

    chore(crud category): replace @autowired by constructor injection in controller and service

--- api/src/main/java/com/marketplace/controller/CategoryController.java
+++ api/src/main/java/com/marketplace/controller/CategoryController.java
@@ -2,18 +2,17 @@ package com.marketplace.controller;
 
 import com.marketplace.entity.Category;
 import com.marketplace.service.impl.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/categories")
 public class CategoryController {
-
-    @Autowired
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @GetMapping
     public ResponseEntity<List<Category>> getAllCategories() {
--- api/src/main/java/com/marketplace/service/impl/CategoryService.java
+++ api/src/main/java/com/marketplace/service/impl/CategoryService.java
@@ -4,16 +4,15 @@ import com.marketplace.entity.Category;
 import com.marketplace.exception.AlreadyExistsException;
 import com.marketplace.exception.NotFoundException;
 import com.marketplace.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
-
-    @Autowired
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
     public List<Category> findAll() {
         return categoryRepository.findAll();
