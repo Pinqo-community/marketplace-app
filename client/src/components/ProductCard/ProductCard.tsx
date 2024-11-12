8ba6d438d3d006ff1a251aa950c1839d67bf6e93
SayliDev <artaxw1@gmail.com>
2024-11-12 12:25:58 +0100

    feat: Add popular products section with product grid layout

--- client/src/components/ProductCard/ProductCard.tsx
+++ client/src/components/ProductCard/ProductCard.tsx
@@ -2,20 +2,19 @@ import { animate, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import bag from "../../assets/icons/bag.svg";
 import star from "../../assets/icons/star.svg";
-import tomate from "../../assets/images/tomate.png";
 import styles from "./ProductCard.module.scss";
+import { ProductProps } from "../../types/Product";
 
-const ProductCard: React.FC = () => {
-  const [quantity, setQuantity] = useState(1);
-  const [displayQuantity, setDisplayQuantity] = useState(1);
+const ProductCard: React.FC<ProductProps> = ({ product }) => {
+  /* -------------------------------------------------------------------------- */
+  /*                                  Statement                                 */
+  /* -------------------------------------------------------------------------- */
 
-  useEffect(() => {
-    const controls = animate(displayQuantity, quantity, {
-      duration: 0.3,
-      onUpdate: (value) => setDisplayQuantity(Math.round(value)),
-    });
-    return controls.stop;
-  }, [quantity, displayQuantity]);
+  const MIN_QUANTITY = 1;
+  const MAX_QUANTITY = 100;
+
+  const [currentQuantity, setCurrentQuantity] = useState(MIN_QUANTITY);
+  const [displayedQuantity, setDisplayedQuantity] = useState(MIN_QUANTITY);
 
   const buttonVariants = {
     initial: { scale: 1 },
@@ -23,82 +22,128 @@ const ProductCard: React.FC = () => {
     tap: { scale: 0.95 },
   };
 
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  /* -------------------------------------------------------------------------- */
+  /*                                  Function                                  */
+  /* -------------------------------------------------------------------------- */
+
+  // Animation de changement de quantité
+  useEffect(() => {
+    const controls = animate(displayedQuantity, currentQuantity, {
+      duration: 0.3,
+      onUpdate: (value) => setDisplayedQuantity(Math.round(value)),
+    });
+    return controls.stop;
+  }, [currentQuantity, displayedQuantity]);
+
+  /* -------------------------------------------------------------------------- */
+  /*                                   Render                                   */
+  /* -------------------------------------------------------------------------- */
   return (
     <motion.article
-      whileHover={{ scale: 1.02 }}
+      variants={itemVariants}
+      whileHover={{
+        scale: 1.03,
+        transition: {
+          duration: 0.1,
+        },
+      }}
       transition={{
-        duration: 0.1,
+        duration: 0.3,
       }}
       className={styles.productCard}
     >
       <div className={styles.imageContainer}>
-        <img src={tomate} alt="Tomate" className={styles.productImage} />
+        <img
+          src={product.image}
+          alt={product.name}
+          className={styles.productImage}
+        />
       </div>
 
       <div className={styles.content}>
         <div className={styles.header}>
-          <h3 className={styles.title}>Tomate</h3>
+          <h3 className={styles.title}>{product.name}</h3>
           <div className={styles.rating}>
             <img src={star} alt="Rating" className={styles.starIcon} />
-            <span className={styles.ratingValue}>4.5</span>
+            <span className={styles.ratingValue}>{product.rating}</span>
           </div>
         </div>
-
-        <div className={styles.prices}>
-          <span className={styles.currentPrice}>20€</span>
-          <span className={styles.originalPrice}>24€</span>
-        </div>
-
-        <div className={styles.stock}>
-          <p>100 ex. en stock</p>
-        </div>
-
-        <div className={styles.actions}>
-          <div className={styles.quantity}>
-            <motion.button
-              className={styles.quantityBtn}
-              aria-label="Diminuer la quantité"
-              disabled={quantity === 1}
-              onClick={() => setQuantity((prevQuantity) => prevQuantity - 1)}
-              variants={buttonVariants}
-              initial="initial"
-              whileTap="tap"
-            >
-              -
-            </motion.button>
-
-            <motion.div
-              className={styles.quantityInput}
-              aria-label="Quantité"
-              key={displayQuantity}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.2 }}
-            >
-              {displayQuantity}
-            </motion.div>
-
-            <motion.button
-              className={styles.quantityBtn}
-              aria-label="Augmenter la quantité"
-              disabled={quantity === 100}
-              onClick={() => setQuantity((prevQuantity) => prevQuantity + 1)}
-              variants={buttonVariants}
-              initial="initial"
-              whileTap="tap"
-            >
-              +
-            </motion.button>
+        <div className={styles.body}>
+          <div className={styles.prices}>
+            <span className={styles.currentPrice}>
+              {product.price.toFixed(2)}€
+            </span>
+            {product.previousPrice && (
+              <span className={styles.originalPrice}>
+                {product.previousPrice.toFixed(2)}€
+              </span>
+            )}
           </div>
 
-          <motion.button
-            className={styles.addToCart}
-            aria-label="Ajouter au panier"
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
+          <div
+            className={`${styles.stock} ${
+              product.stock < 10 ? styles.low : ""
+            }`}
           >
-            <img src={bag} alt="bag" className={styles.bagIcon} />
-          </motion.button>
+            <p>{product.stock} ex. en stock</p>
+          </div>
+
+          <div className={styles.actions}>
+            <div className={styles.quantity}>
+              <motion.button
+                className={styles.quantityBtn}
+                aria-label="Diminuer la quantité"
+                disabled={currentQuantity === MIN_QUANTITY}
+                onClick={() =>
+                  setCurrentQuantity((prevQuantity) => prevQuantity - 1)
+                }
+                variants={buttonVariants}
+                initial="initial"
+                whileTap="tap"
+              >
+                -
+              </motion.button>
+
+              <motion.div
+                className={styles.quantityInput}
+                aria-label="Quantité"
+                key={displayedQuantity}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.2 }}
+              >
+                {displayedQuantity}
+              </motion.div>
+
+              <motion.button
+                className={styles.quantityBtn}
+                aria-label="Augmenter la quantité"
+                disabled={currentQuantity === MAX_QUANTITY}
+                onClick={() =>
+                  setCurrentQuantity((prevQuantity) => prevQuantity + 1)
+                }
+                variants={buttonVariants}
+                initial="initial"
+                whileTap="tap"
+              >
+                +
+              </motion.button>
+            </div>
+
+            <motion.button
+              className={styles.addToCart}
+              aria-label="Ajouter au panier"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <img src={bag} alt="bag" className={styles.bagIcon} />
+            </motion.button>
+          </div>
         </div>
       </div>
     </motion.article>
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,101 @@
 import Map from "../../components/Map/Map";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import MainLayout from "../../layouts/MainLayout";
 import styles from "./HomePage.module.scss";
+import tomate from "../../assets/images/tomate.png";
+import { motion } from "framer-motion";
+import arrowIcon from "../../assets/icons/arrow.svg";
 
 const HomePage: React.FC = () => {
   /* -------------------------------------------------------------------------- */
   /*                                  Statement                                 */
   /* -------------------------------------------------------------------------- */
+  const products = [
+    {
+      image:
+        "https://plus.unsplash.com/premium_photo-1663957861996-8093b48a22e6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG9uZXl8ZW58MHx8MHx8fDA%3D",
+      name: "Miel de fleurs sauvages",
+      rating: 4.7,
+      price: 12.99,
+      previousPrice: 14.99,
+      stock: 7,
+    },
 
+    {
+      image:
+        "https://images.unsplash.com/photo-1542820191-bdc08051351b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fE9yZ2FuaWMlMjBhcHBsZXxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Pomme bio du verger",
+      rating: 4.2,
+      price: 1.2,
+      previousPrice: null,
+      stock: 51,
+    },
+
+    {
+      image:
+        "https://plus.unsplash.com/premium_photo-1668616815449-b61c3f4d4f44?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fG9saXZlJTIwb2lsfGVufDB8fDB8fHww",
+      name: "Huile d'olive extra vierge",
+      rating: 4.8,
+      price: 15.49,
+      previousPrice: 16.99,
+      stock: 27,
+    },
+    {
+      image:
+        "https://img-3.journaldesfemmes.fr/ZfmzxO5Kyg0e3j1URh4V8Mf3slc=/1500x/smart/097777a79f144a048f7008573f8584d5/ccmcms-jdf/27424516.jpg",
+      name: "Pomme de terre bio",
+      rating: 4.7,
+      price: 3.99,
+      previousPrice: null,
+      stock: 32,
+    },
+
+    {
+      image:
+        "https://images.unsplash.com/photo-1513088222195-4388e0a0c553?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGphbXxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Confiture de fraise artisanale",
+      rating: 4.3,
+      price: 6.5,
+      previousPrice: null,
+      stock: 15,
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1522249341405-3871994ac062?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fERhcmslMjBjaG9jb2xhdGV8ZW58MHx8MHx8fDA%3D",
+      name: "Chocolat noir bio 80%",
+      rating: 4.4,
+      price: 3.5,
+      previousPrice: 4.0,
+      stock: 18,
+    },
+    {
+      image: tomate,
+      name: "Tomate Fandango",
+      rating: 4.7,
+      price: 2.9,
+      previousPrice: 3.35,
+      stock: 72,
+    },
+    {
+      image:
+        "https://plus.unsplash.com/premium_photo-1700767180790-f24ec173ba2e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTEzfHxXaG9sZW1lYWwlMjBicmVhZHxlbnwwfHwwfHx8MA%3D%3D",
+      name: "Pain complet aux céréales",
+      rating: 4.5,
+      price: 2.99,
+      previousPrice: 3.5,
+      stock: 3,
+    },
+  ];
+
+  const listVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
   /* -------------------------------------------------------------------------- */
   /*                                  Function                                  */
   /* -------------------------------------------------------------------------- */
@@ -16,12 +105,35 @@ const HomePage: React.FC = () => {
   /* -------------------------------------------------------------------------- */
   return (
     <MainLayout>
-      <section className={styles.section}>
-        <div className={styles.title}>
-          <h2>Les producteurs près de chez vous</h2>
+      <section className={styles.popularProducts} aria-labelledby="popular-products-title">
+        <div className={styles.sectionContainer}>
+          <div className={styles.titleContainer}>
+            <h2 className={styles.title}>Produits populaires</h2>
+            <div className={styles.moreInfo}>
+              <div className={styles.moreInfoText}>Voir tout</div>
+              <img src={arrowIcon} alt="arrow" />
+            </div>
+          </div>
+
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={listVariants}
+            className={styles.productsGrid}
+          >
+            {products.map((product) => (
+              <ProductCard key={product.name} product={product} />
+            ))}
+          </motion.div>
         </div>
-        <div className={styles.mapContainer}>
-          <Map />
+      </section>
+
+      <section className={styles.localProducers} aria-labelledby="local-producers-title">
+        <div className={styles.sectionContainer}>
+          <h2 className={styles.title}>Les producteurs près de chez vous</h2>
+          <div className={styles.mapWrapper}>
+            <Map />
+          </div>
         </div>
       </section>
     </MainLayout>
