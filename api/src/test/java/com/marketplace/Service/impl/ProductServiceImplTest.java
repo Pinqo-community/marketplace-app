42df99ff356e47de2af169477cd19cd6cfd56e31
moria <isabelle.moriamez@orange.fr>
2024-12-02 11:00:34 +0100

    refactoring TU, use WebMvc for ProductControllerTest and cleaning code

--- api/src/test/java/com/marketplace/Service/impl/ProductServiceImplTest.java
+++ api/src/test/java/com/marketplace/Service/impl/ProductServiceImplTest.java
@@ -16,9 +16,6 @@ import org.mockito.InjectMocks;
 import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 
 import java.util.Collections;
 import java.util.List;
