0cdd3bcc080a5a6ecea9782851fc02dc693eb152
SayliDev <artaxw1@gmail.com>
2024-12-12 23:06:33 +0100

    fix: removed unused import causing Docker build issue

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,13 +1,7 @@
-// External libraries
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer } from "react-leaflet";
-import "react-leaflet-markercluster/dist/styles.min.css";
-
-// Assets and styles
 import "leaflet.markercluster/dist/leaflet.markercluster";
 import styles from "./Map.module.scss";
-
-// Types and Components
 import { useRef, useState } from "react";
 import { Producer } from "../../types/Producer";
 import ClusterMarkers from "./ClusterMarkers";
