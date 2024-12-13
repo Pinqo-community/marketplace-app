70d713c52ecb59636d8ea9a5e206501b68b857d3
moria <isabelle.moriamez@orange.fr>
2024-12-13 09:19:43 +0100

    adjust compose.yaml and Map.tsx

--- client/src/components/Map/Map.tsx
+++ client/src/components/Map/Map.tsx
@@ -1,7 +1,7 @@
 // External libraries
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer } from "react-leaflet";
-import "react-leaflet-markercluster/dist/styles.min.css";
+
 
 // Assets and styles
 import "leaflet.markercluster/dist/leaflet.markercluster";
