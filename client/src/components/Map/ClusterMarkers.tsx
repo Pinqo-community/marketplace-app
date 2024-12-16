4fed0eb611f1d41d815ad2f965c4738366081330
SayliDev <artaxw1@gmail.com>
2024-12-16 19:38:52 +0100

    fix(types): resolve TypeScript error for 'markerClusterGroup' property

--- client/src/components/Map/ClusterMarkers.tsx
+++ client/src/components/Map/ClusterMarkers.tsx
@@ -1,4 +1,5 @@
 import L from "leaflet";
+import "leaflet.markercluster";
 import { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import arrowIcon from "../../assets/icons/arrow.svg";
@@ -26,8 +27,8 @@ const ClusterMarkers = ({ producers }: { producers: Producer[] }) => {
               <h3 class="${styles.name}">${producer.name}</h3>
             </div>
            <div class="${styles.producerStatus} ${
-        producer.isOpen ? styles.open : styles.closed
-      }">
+             producer.isOpen ? styles.open : styles.closed
+           }">
             <div class="${styles.dot}"></div>
             <div class="${styles.statusText}">
               ${producer.isOpen ? "Disponible" : "Indisponible"}
