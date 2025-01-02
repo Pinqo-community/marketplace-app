0d6dd345ebc23e911b4034d5df7124704c411e62
SayliDev <artaxw1@gmail.com>
2025-01-02 12:49:19 +0100

    fix(homepage, location-popup): apply bug fixes and UI adjustments

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,13 +1,20 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import { motion } from "framer-motion";
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="main-layout">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+      className="main-layout"
+    >
       <Header />
       <main>{children}</main>
       <Footer />
-    </div>
+    </motion.div>
   );
 };
 
