538af8540fd2309ce34a655be41fa02dbdd7a38f
SayliDev <artaxw1@gmail.com>
2025-02-08 18:52:41 +0100

    feat(auth): add signup page structure and input validation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
     </Routes>
   );
 }
