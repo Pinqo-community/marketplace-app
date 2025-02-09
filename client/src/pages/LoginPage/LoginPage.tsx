7c1444ab8f532febff095ea084edb8ff104a7c64
SayliDev <artaxw1@gmail.com>
2025-02-09 14:03:46 +0100

    feat(auth): add login functionality with form handling and API call
    - Implement login form with email & password inputs
    - Add state management with useState for controlled inputs
    - Handle login submission with API call and error handling
    - Use Redux dispatch to update authentication state
    - Redirect user to dashboard on successful login
    - Display error message on failed login attempt

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -3,38 +3,67 @@ import { AuthLayout } from "@/components/Auth/AuthLayout";
 import { AuthInput } from "@/components/Auth/AuthInput";
 import { SocialButtons } from "@/components/Auth/SocialButtons";
 import styles from "@/components/Auth/Auth.module.scss";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { login } from "@/services/authService";
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(""); // Reset l'erreur avant chaque tentative
+    try {
+      await login(dispatch, { email, password });
+      console.log("Connexion reussi");
+
+      navigate("/"); // Envoie l'utilisateur vers la page d'accueil
+    } catch (err: any) {
+      setError("Échec de la connexion. Vérifiez vos identifiants.");
+      console.error("Erreur de connexion", err);
+    }
+  };
 
   return (
     <AuthLayout title="Se connecter">
-      <div className={styles.formGroup}>
-        <AuthInput
-          type="text"
-          name="email"
-          placeholder="Email"
-          value=""
-          onChange={() => {}}
-        />
-        <AuthInput
-          type="password"
-          name="password"
-          placeholder="Mot de passe"
-          value=""
-          onChange={() => {}}
-        />
-        <div className={styles.checkboxContainer}>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" />
-            Se souvenir de moi
-          </label>
-          <a className={styles.forgotPassword} href="">
-            Mot de passe oublié ?
-          </a>
+      <form onSubmit={handleLogin}>
+        <div className={styles.formGroup}>
+          {error && (
+            <p className={`${styles.errorMessage} ${styles.errorCenter}`}>
+              {error}
+            </p>
+          )}
+          <AuthInput
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <AuthInput
+            type="password"
+            name="password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className={styles.checkboxContainer}>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" />
+              Se souvenir de moi
+            </label>
+            <a className={styles.forgotPassword}>Mot de passe oublié ?</a>
+          </div>
         </div>
-      </div>
-      <button className={styles.authButton}>Se connecter</button>
+        <button type="submit" className={styles.authButton}>
+          Se connecter
+        </button>
+      </form>
       <p className={styles.switchAuthText}>
         Pas encore de compte ?{" "}
         <a onClick={() => navigate("/signup")}>S'inscrire</a>
